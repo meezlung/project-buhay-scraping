b6deeef726fc950391010c7f14fd632bcde2e4da
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-11-04 13:42:31 +0800

    Added district number feature

--- phil_atlas_scraper.py
+++ phil_atlas_scraper.py
@@ -7,9 +7,10 @@ import json, csv
 FIELD_NAME: TypeAlias = str
 
 class PhilAtlas:
-    def __init__(self) -> None:
+    def __init__(self, barangay_districts: dict[int, list[str]]) -> None:
         self.url = 'https://www.philatlas.com/luzon/ncr/quezon-city.html'
         self.barangays: list[dict[FIELD_NAME, str | int | float]] = []
+        self.barangay_districts = barangay_districts
 
     def main(self) -> None:
         response = self.get_url_response(self.url)
@@ -46,7 +47,7 @@ class PhilAtlas:
 
                 # Fields
                 barangay_name: str = self.get_barangay_name(row) 
-                district_number: int = -1
+                district_number: int = self.get_district_number(barangay_name)
                 population_data_2015, population_data_2020 = self.get_population_data(row)
                 household_population_2015: int = -1
                 household_count_2015: int = -1
@@ -99,6 +100,25 @@ class PhilAtlas:
             barangay = barangay_tag.get_text(strip=True)
             return barangay
         return ""
+    
+    def get_district_number(self, barangay_name: str) -> int:
+        # This is for barangays that have different formatting
+        if barangay_name == 'Santo Cristo' or barangay_name == 'Santo Domingo' or barangay_name == 'Sienna' or barangay_name == 'N. S. Amoranto':
+            return 1
+        elif barangay_name == 'Balong Bato':
+            return 2
+        elif barangay_name == 'Saint Ignatius' or barangay_name == 'Claro':
+            return 3
+        elif barangay_name == 'Teachers Village East' or barangay_name == 'Teachers Village West' or barangay_name == 'Aurora' or barangay_name == 'San Isidro':
+            return 4
+        elif barangay_name == 'Santa Lucia' or barangay_name == 'Santa Cruz' or barangay_name == 'Santa Monica':
+            return 5
+      
+        for district_number, barangays in self.barangay_districts.items():
+            if barangay_name in barangays:
+                return district_number
+
+        return -1
 
     def get_population_data(self, row: Tag) -> tuple[int, int]:
         cells = row.find_all('td')
@@ -129,7 +149,7 @@ class PhilAtlas:
     def append_all_data(self, barangay_name: str, district_number: int, population_data_2015: int, population_data_2020: int, household_population_2015: int, household_count_2015: int, average_household_size_2015: float) -> None:
         self.barangays.append({
             'barangay_name': barangay_name,
-            'district_number': -1,
+            'district_number': district_number,
             'population_data_2015': population_data_2015,
             'population_data_2020': population_data_2020,
             'household_population_2015': household_population_2015,
@@ -153,8 +173,43 @@ class PhilAtlas:
             for barangay in self.barangays:
                 writer.writerow(barangay)
 
+
+class WikipediaBarangay(PhilAtlas):
+    def __init__(self) -> None:
+        self.url = 'https://en.wikipedia.org/wiki/List_of_barangays_in_Quezon_City'
+        self.districts: dict[int, list[str]] = {}
+
+    def main(self) -> None:
+        response = self.get_url_response(self.url)
+        soup = self.parse_html_from_response(response)
+        self.extract_districts(soup)
+
+    def extract_districts(self, soup: BeautifulSoup) -> None:
+        district_tables = soup.find_all('table', {'class': 'wikitable'})
+        district_number = 1
+
+        for table in district_tables:
+            rows = table.find_all('tr')
+            for row in rows:
+                cols = row.find_all('td')
+
+                if cols:
+                    if district_number in self.districts:
+                        self.districts[district_number].append(cols[0].text.strip())
+                    else:
+                        self.districts[district_number] = [cols[0].text.strip()]
+
+            district_number += 1
+
+    def display_districts(self) -> None:
+        print(json.dumps(self.districts, indent=4))
+
+
 if __name__ == '__main__':
-    phil_atlas = PhilAtlas()
+    wikipedia = WikipediaBarangay()
+    wikipedia.main()
+
+    phil_atlas = PhilAtlas(wikipedia.districts)
     phil_atlas.main()
     phil_atlas.print_json()
     phil_atlas.write_to_json()
