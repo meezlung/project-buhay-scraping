d097f69fae18b0c2903c5436c48a3e5dba8a8303
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-11-06 22:15:28 +0800

    Refactored code for phil_atlas_scraper

--- phil_atlas_scraper.py
+++ phil_atlas_scraper.py
@@ -9,11 +9,13 @@ FIELD_NAME: TypeAlias = str
 
 class PhilAtlas:
     def __init__(self, barangay_districts: dict[int, list[str]]) -> None:
+        """ Initialize the main URL and data containers """
         self.url = 'https://www.philatlas.com/luzon/ncr/quezon-city.html'
         self.barangays: list[dict[FIELD_NAME, str | int | float]] = []
         self.barangay_districts = barangay_districts
 
     def main(self) -> None:
+        """ Main function to run the scraper """
         response = self.get_url_response(self.url)
         soup = self.parse_html_from_response(response)
         table = self.get_table(soup)
@@ -26,27 +28,32 @@ class PhilAtlas:
             print("Table not found or is not a valid tag.")
 
     def get_url_response(self, url: str) -> Response:
+        """ Fetches response from a given URL """
         response = requests.get(url)
         response.raise_for_status()
         return response
 
     def parse_html_from_response(self, response: Response) -> BeautifulSoup:
+        """ Parses HTML content from a response """
         soup = BeautifulSoup(response.text, 'html.parser')
         return soup
     
     def get_table(self, soup: BeautifulSoup) -> Tag | NavigableString | None:
+        """ Finds and returns the main table containing all barangays with their information """
         table = soup.find('table', {'class': 'sortable datatable', 'id': 'lguTable'})
         return table
     
     def get_barangay_table_body(self, table: Tag) -> Tag | NavigableString | None:
+        """ Returns the table body of the main table """
         tbody = table.find('tbody')
         return tbody
 
     def get_barangay_information(self, tbody: Tag | NavigableString | None) -> None:
+        """ Extracts information from each barangay """
         if isinstance(tbody, Tag):
             for row in tbody.find_all('tr'):
 
-                # Fields
+                # Extract each field from the row
                 barangay_name: str = self.get_barangay_name(row) 
                 district_number: int = self.get_district_number(barangay_name)
                 population_data_2015, population_data_2020 = self.get_population_data(row)
@@ -65,12 +72,14 @@ class PhilAtlas:
 
                 else:
                     print("Demographics table not found or is not a valid tag.")
+                    self.append_all_data(barangay_name, district_number, population_data_2015, population_data_2020, household_population_2015, household_count_2015, average_household_size_2015)
 
         else:
             print("tbody not found or is not a valid tag.")
             return None
         
     def get_barangay_link(self, row: Tag) -> str:
+        """ Retrieves the barangay-specific link for each row """
         barangay_tag = row.find('th')
 
         if barangay_tag:
@@ -85,6 +94,7 @@ class PhilAtlas:
         return ""
     
     def open_barangay_link(self, row: Tag) -> Tag | NavigableString | None:
+        """ Opens the barangay-specific link and parses the page for demographics data """
         barangay_link = self.get_barangay_link(row)
         barangay_response = self.get_url_response(barangay_link)
         barangay_soup = self.parse_html_from_response(barangay_response)
@@ -92,10 +102,12 @@ class PhilAtlas:
         return barangay_demographics
 
     def find_barangay_demographics(self, barangay_soup: BeautifulSoup) -> Tag | NavigableString | None:
+        """ Looks for the demographics table with graphs in the barangay-specific page """
         barangay_demographics = barangay_soup.find('table', {'class': 'chart-table', 'id': 'households-table'})
         return barangay_demographics 
     
     def get_barangay_name(self, row: Tag) -> str:
+        """ Extracts the barangay name from the row """
         barangay_tag = row.find('th')
         if barangay_tag:
             barangay = barangay_tag.get_text(strip=True)
@@ -103,7 +115,9 @@ class PhilAtlas:
         return ""
     
     def get_district_number(self, barangay_name: str) -> int:
-        # This is for barangays that have different formatting
+        """ Determines the district number of the barangay """
+
+        # This are special cases where the barangay name from PhilAtlas is different from the one in Wikipedia
         if barangay_name == 'Santo Cristo' or barangay_name == 'Santo Domingo' or barangay_name == 'Sienna' or barangay_name == 'N. S. Amoranto':
             return 1
         elif barangay_name == 'Balong Bato':
@@ -122,6 +136,7 @@ class PhilAtlas:
         return -1
 
     def get_population_data(self, row: Tag) -> tuple[int, int]:
+        """ Extracts the population data (2015 and 2020) from the row """
         cells = row.find_all('td')
         population_data_2020_str = cells[1].get_text(strip=True)
         population_data_2015_str = cells[2].get_text(strip=True)
@@ -131,6 +146,7 @@ class PhilAtlas:
         return population_data_2015, population_data_2020
     
     def get_demographics_data(self, barangay_demographics: Tag) -> tuple[int, int, float]:
+        """ Extracts household demographics data from the table (2015 data) """
         demographics_rows = barangay_demographics.find_all('tr')
         last_row = demographics_rows[-1] # Last row always contains the 2015 data
         cells = last_row.find_all('td')
@@ -145,9 +161,11 @@ class PhilAtlas:
         return household_population_2015, household_count_2015, average_household_size_2015
 
     def convert_str_to_int(self, string_number: str) -> int: 
+        """ Converts a string number to an integer"""
         return int(string_number.replace(',', ''))
 
     def append_all_data(self, barangay_name: str, district_number: int, population_data_2015: int, population_data_2020: int, household_population_2015: int, household_count_2015: int, average_household_size_2015: float) -> None:
+        """ Appends all collected data to the barangays list """
         self.barangays.append({
             'barangay_name': barangay_name,
             'district_number': district_number,
@@ -159,13 +177,16 @@ class PhilAtlas:
         })
 
     def print_json(self) -> None:
+        """ Prints the collected data in JSON format for debugging purposes """
         print(json.dumps(self.barangays, indent=4))
 
     def write_to_json(self) -> None:
+        """  Writes the collected data to a JSON file """
         with open('barangays.json', 'w') as jsonfile:
             json.dump(self.barangays, jsonfile, indent=3)        
 
     def write_to_csv(self) -> None:
+        """ Writes the collected data to a CSV file """
         with open('barangays.csv', 'w', newline='') as csvfile:
             fieldnames = self.barangays[0].keys()
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -177,15 +198,18 @@ class PhilAtlas:
 
 class WikipediaBarangay(PhilAtlas):
     def __init__(self) -> None:
+        """ Initialize Wikipedia URL for the district numbers of data dictionary """
         self.url = 'https://en.wikipedia.org/wiki/List_of_barangays_in_Quezon_City'
         self.districts: dict[int, list[str]] = {}
 
     def main(self) -> None:
+        """ Main method to fetch and parse barangay districts from Wikipedia """
         response = self.get_url_response(self.url)
         soup = self.parse_html_from_response(response)
         self.extract_districts(soup)
 
     def extract_districts(self, soup: BeautifulSoup) -> None:
+        """ Extracts the barangay districts from the Wikipedia page and organizes them per district number via dictionary """
         district_tables = soup.find_all('table', {'class': 'wikitable'})
         district_number = 1
 
@@ -203,6 +227,7 @@ class WikipediaBarangay(PhilAtlas):
             district_number += 1
 
     def display_districts(self) -> None:
+        """  Prints the barangay districts per district number for debugging purposes """
         print(json.dumps(self.districts, indent=4))
 
 
